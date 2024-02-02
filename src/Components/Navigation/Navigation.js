391b868d5930cd17a4235d1eb06db65ac19e5766
Drishti Saraf <drishtisaraf0307@gmail.com>
2024-02-02 22:32:31 +0530

    poultry ui

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import  styles from './Navbar.module.css';
+import logo from '../../assessts/logo.svg'
+import Menu from '../../assessts/Menu.svg'
+import Profile from '../../assessts/Profile.svg'
+import Search from '../../assessts/Search.svg'
+
+const Navigation = () => {
+  return (
+    <div className='Navigation'>
+
+        <header className='App-header'>
+
+            <nav className={`${styles.navbar}`}>
+                <a href ='/' className={`${styles.navLogo}`}>
+                    <img src={logo}/>
+                </a>
+<ul>
+    <li>OUR PRODUCTS</li>
+    <li>CARE EGGS</li>
+    <li>ABOUT</li>
+    <li>CONTACT</li>
+</ul>
+
+<div className={`${styles.sideContent}`}>
+    <div className={`${styles.sideText}`}>
+    <p>Customer Care:</p>
+    <p>+91 9988776655</p>
+    </div>
+
+    <div className={`${styles.sideImages}`}>
+              <img src={Search} alt="Search" /> 
+              <img src={Menu} alt="Menu" />
+              <img src={Profile} alt="Profile" /> 
+             
+ 
+    </div>
+</div>
+
+            </nav>
+
+        </header>
+
+    </div>
+  )
+}
+
+export default Navigation
